9f424e0deda1f74c383149515d340f150a75f44c
Stephen Ulrich <scu@pobox.com>
2016-02-14 16:46:54 -0600

    improved display

--- app/components/game-state-controller/game-state-controller.js
+++ app/components/game-state-controller/game-state-controller.js
@@ -3,26 +3,48 @@
 // Declare app level module which depends on views, and components
 angular.module('lifeByScu.gameStateController',[]).
 
-controller('gameStateController',['$scope','$window',function($scope,$window){
+controller('gameStateController',['$scope','$timeout',function($scope,$timeout){
+	$scope.promise = null;
+	$scope.rows = 50;
+	$scope.columns = 50;
+	$scope.density = 0.15;
+	$scope.delay = 150;
+	$scope.steps = 300;
 
-	$scope.create_board = function(x,y,density){
-		var new_board = [];
-		for(var i=0; i<y; i++){
-			new_board[i]=[];
-			for(var j=0; j<x; j++){
+	$scope.create_board = function(rows, columns, density){
+		if(rows > 200){
+			rows = 200;
+		}
+		if (columns > 200){
+			columns=200;
+		}
+		var new_board = new Array(rows);
+		for(var i=0; i<rows; i++){
+			new_board[i]= new Array(columns);
+			for(var j=0; j<columns; j++){
 				new_board[i][j] = {is_alive: Math.random()<density}
 			}
 		}
 		$scope.board=new_board;
-		$scope.max_x=x;
-		$scope.max_y=y;
+		$scope.max_x=columns;
+		$scope.max_y=rows;
 	}
 
-	$scope.create_board(50,50,.15);
+	$scope.create_board(50,50,0);
 
-	$scope.iterateState = function(){
-		console.log($scope.msec, $scope.steps)
-		while($scope.steps>0){ 
+
+	$scope.toggle_play = function(){
+
+		if($scope.promise==null){
+			$scope.iterateState($scope.steps,$scope.delay);
+		} else {
+			$timeout.cancel($scope.promise);
+			$scope.promise = null;
+		}
+	}
+
+	$scope.iterateState = function(steps,delay){
+		if(steps>0){
 			var next_board = [];
 			$scope.board.forEach(function(element, index, array){
 				next_board[index] = [];
@@ -31,8 +53,14 @@ controller('gameStateController',['$scope','$window',function($scope,$window){
 				});
 			});
 			$scope.board = next_board;
-			$scope.steps -= 1;
-
+			steps -= 1;
+			if(steps==0){
+				$scope.promise==null
+			} else {
+				$scope.promise = $timeout($scope.iterateState,delay,true,steps,delay)
+			}
+		} else {
+			$scope.promise==null
 		}
 	}
 
